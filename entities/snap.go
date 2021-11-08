b0d2f3080fc70f768ab12271a6258f024da08ddb
Gabriel Antunes <gabriel.antunes@compasso.com.br>
2021-11-08 16:29:42 -0300

    remove firebase auth

--- entities/snap.go
+++ entities/snap.go
@@ -6,6 +6,6 @@ type Snap struct {
 	gorm.Model
 	FileName string `json:"fileName" gorm:"type:varchar(255);not null"`
 	SnapUrl  string `json:"snapUrl" gorm:"type:varchar(255)"`
-	AlbumID  uint
+	AlbumID  uint   `json:"albumId"`
 	Album    Album `json:"album" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
--- snaps/create_snap_dto.go
+++
@@ -1,5 +0,0 @@
-package snaps
-
-type CreateSnapDTO struct {
-	AlbumId uint `json:"albumId"`
-}
